1d5774bc32572e165e272dd6a68c01fde53a4db9
Ben Kietzman <ben@kietzman.org>
2022-08-15 12:37:38 -0500

    Saved work.

--- cup/addrInfo.cpp
+++ cup/addrInfo.cpp
@@ -243,12 +243,12 @@ bool initialize(string strPrefix, int argc, char *argv[], string &strError)
     bResult = true;
     // {{{ set signal handling
     sethandles(sighandle);
-    sigignore(SIGBUS);
-    sigignore(SIGCHLD);
-    sigignore(SIGCONT);
-    sigignore(SIGPIPE);
-    sigignore(SIGSEGV);
-    sigignore(SIGWINCH);
+    signal(SIGBUS, SIG_IGN);
+    signal(SIGCHLD, SIG_IGN);
+    signal(SIGCONT, SIG_IGN);
+    signal(SIGPIPE, SIG_IGN);
+    signal(SIGSEGV, SIG_IGN);
+    signal(SIGWINCH, SIG_IGN);
     // }}} 
     // {{{ command line arguments
     for (int i = 1; i < argc; i++)
--- cup/copy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-prefix=/usr/local
-
-if [ "${2}" != '' ]; then
-  prefix=$2
-fi
-
-install $1 ${prefix}/acorn/cup/
